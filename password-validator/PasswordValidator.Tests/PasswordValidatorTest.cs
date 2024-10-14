d6f7669ed16ffc09c22b708a9ba818d6029d28c0
Jaroslav Markovec <Jaroslav.Markovec@tecalliance.net>
2024-10-14 16:45:38 +0100

    Complete the password validato tests

--- password-validator/PasswordValidator.Tests/PasswordValidatorTest.cs
+++ password-validator/PasswordValidator.Tests/PasswordValidatorTest.cs
@@ -3,12 +3,116 @@ using NUnit.Framework;
 
 namespace PasswordValidator.Tests
 {
+
+    /*
+ - Have more than 8 characters
+ - Contains a capital letter
+ - Contains a lowercase
+ - Contains a number
+ - Contains an underscore
+    */
+
+
     public class PasswordValidatorTest
     {
-        [Test]
-        public void Think_A_Good_Name_And_Change_It()
+        private PasswordValidator _validator;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.That(false, Is.True);
+             _validator = new PasswordValidator();
         }
+
+
+        [Test]
+        public void Password_is_valid()
+        {
+            
+            var password = "A17_patat";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Short_Password_IsInvalid()
+        {
+            
+            var password = "A17_pata";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Password_WithoutUppercase_IsInvalid()
+        {
+
+            var password = "password_123";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+
+        [Test]
+        public void Password_WithoutLowercase_IsInvalid()
+        {
+
+            var password = "PASSWORD_123";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Password_WithoutNumber_IsInvalid()
+        {
+
+            var password = "Password_patata";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Password_WithoutUnderscore_IsInvalid()
+        {
+
+            var password = "Password123";
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Null_Password_IsInvalid()
+        {
+
+            string password = null;
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
+
+        [Test]
+        public void Empty_Password_IsInvalid()
+        {
+            
+            string password = string.Empty;
+
+            var result = _validator.Validate(password);
+
+            Assert.That(result, Is.False);
+        }
+
     }
 }
