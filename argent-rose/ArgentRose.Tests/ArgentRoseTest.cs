d7eb40d21f727e59f57e2c3e176dd98857bba7b3
Jaume Borras Rossello <jaume.borras@toolfactory.net>
2024-10-18 09:34:41 +0200

    feature: argentrose incomplete

--- argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
+++ argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
@@ -1,14 +1,59 @@
 
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace ArgentRose.Tests
 {
     public class ArgentRoseTest
     {
-        [Test]
-        public void Think_A_Good_Name_And_Change_It()
+
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.That(false, Is.True);
+
+        }
+
+        [Test]
+        public void Update_EmptyInventory()
+        {
+            Inventory inventory = new();
+
+            inventory.Update();
+            
+            Inventory expectedInventory = new Inventory();
+            Assert.That(inventory, Is.EqualTo(expectedInventory));
+        }
+
+        [Test]
+        public void Update_Regular()
+        {
+            Inventory inventory = new();
+            Product regularProduct = new Product("Corcho", 3, 6);
+
+            inventory.AddProduct(regularProduct);
+
+            inventory.Update();
+
+            Inventory expectedInventory = new Inventory();
+            expectedInventory.AddProduct(new Product("Corcho", 1, 5));
+            Assert.That(inventory, Is.EqualTo(expectedInventory));
+        }
+
+        [Test]
+        public void Update_Regulars()
+        {
+            Inventory inventory = new();
+
+            inventory.AddProduct(new Product("Corcho", 3, 6));
+            inventory.AddProduct(new Product("Corcho", 3, 6));
+
+            inventory.Update();
+
+            Inventory expectedInventory = new Inventory();
+            expectedInventory.AddProduct(new Product("Corcho", 1, 5));
+            expectedInventory.AddProduct(new Product("Corcho", 1, 5));
+            Assert.That(inventory, Is.EqualTo(expectedInventory));
         }
     }
 }
---
+++ argent-rose/ArgentRose/Inventory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArgentRose
+{
+    public class Inventory
+    {
+        private List<Product> _products = new List<Product>();
+
+        public void Update()
+        {
+            foreach (var product in _products)
+            {
+                UpdateRegular(product);
+            }
+        }
+
+        private void UpdateRegular(Product product)
+        {
+            product.Sellin -= 1;
+            product.Quality -= 2;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", _products);
+        }
+
+        protected bool Equals(Inventory other)
+        {
+            return _products.SequenceEqual(other._products);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Inventory)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_products != null ? _products.GetHashCode() : 0);
+        }
+
+        public void AddProduct(Product product)
+        {
+            this._products.Add(product);
+        }
+    }
+
+    public class Product
+    {
+        public string Description { get; set; }
+        public int Quality { get; set; }
+        public int Sellin { get; set; }
+
+        public Product(string description, int quality, int sellin)
+        {
+            Description = description;
+            Quality = quality;
+            Sellin = sellin;
+        }
+
+        protected bool Equals(Product other)
+        {
+            return Description == other.Description && Quality == other.Quality && Sellin == other.Sellin;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Product)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Description, Quality, Sellin);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Description)}: {Description}, {nameof(Quality)}: {Quality}, {nameof(Sellin)}: {Sellin}";
+        }
+    }
+}
